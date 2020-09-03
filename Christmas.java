e017251dee04fa767dacaeb5900963fd368ab614
kksheth <62899864+kksheth@users.noreply.github.com>
2020-09-03 12:24:29 -0700

    Create Christmas.java

---
+++ Christmas.java
@@ -0,0 +1,108 @@
+//This program prints out the lines to the song "On the First Day of Christmas" 
+
+public class Christmas {
+   public static void trueLove() {
+      System.out.println("my true love sent to me");
+   }
+   
+   public static void first(){
+      System.out.println("a partridge in a pear tree.");
+      System.out.println();
+   }
+   
+   public static void second(){
+      System.out.println("two turtle doves, and");
+      first();
+   }
+   
+   public static void third(){
+      System.out.println("three French hens,");
+      second();  
+   }
+   
+   public static void fourth(){
+      System.out.println("four calling birds,");
+      third();  
+   }
+   
+   public static void fifth(){
+      System.out.println("five golden rings,");
+      fourth();  
+   }
+   
+   public static void sixth(){
+      System.out.println("six geese a-laying,");
+      fifth();  
+   }
+   
+   public static void seventh(){
+      System.out.println("seven swans a-swimming,");
+      sixth();  
+   }
+   
+   public static void eighth(){
+      System.out.println("eight maids a-milking,");
+      seventh();  
+   }
+   
+   public static void ninth(){
+      System.out.println("nine ladies dancing,");
+      eighth();  
+   }
+   
+   public static void tenth(){
+      System.out.println("ten lords a-leaping,");
+      ninth();  
+   }
+   
+   public static void eleventh(){
+      System.out.println("eleven pipers piping,");
+      tenth();  
+   }
+   
+   public static void twelfth(){
+      System.out.println("Twelve drummers drumming,");
+      eleventh();  
+   }
+   
+   public static void main(String[] args){
+      System.out.println("On the first day of Christmas,");
+      trueLove();
+      first();
+      System.out.println("On the second day of Christmas,");
+      trueLove();
+      second();
+      System.out.println("On the third day of Christmas,");
+      trueLove();
+      third();
+      System.out.println("On the fourth day of Christmas,");
+      trueLove();
+      fourth();
+      System.out.println("On the fifth day of Christmas,");
+      trueLove();
+      fifth();
+      System.out.println("On the sixth day of Christmas,");
+      trueLove();
+      sixth();
+      System.out.println("On the seventh day of Christmas,");
+      trueLove();
+      seventh();
+      System.out.println("On the eighth day of Christmas,");
+      trueLove();
+      eighth();
+      System.out.println("On the ninth day of Christmas,");
+      trueLove();
+      ninth();
+      System.out.println("On the tenth day of Christmas,");
+      trueLove();
+      tenth();
+      System.out.println("On the eleventh day of Christmas,");
+      trueLove();
+      eleventh();
+      System.out.println("On the twelfth day of Christmas,");
+      trueLove();
+      twelfth();
+      
+   }
+   
+}
